43c86b089624110c8f80b4190ee7307d636480b6
t <t>
2023-11-10 07:51:02 -0600

    adding tests

---
+++ test/test.js
@@ -0,0 +1,15 @@
+test('test #1', () => {
+  expect(1).toBe(1);
+});
+
+test('test #2', () => {
+  expect(2).toBe(2);
+});
+
+test('test #3', () => {
+  expect(3).toBe(3);
+});
+
+test('bad test #1', () => {
+  expect(1).toBe(2);
+});
