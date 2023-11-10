edd1db9b8b60b2d320da7f30c7b6c87a772d9de9
t <t>
2023-11-10 07:43:33 -0600

    initial good commit - break later

---
+++ index.js
@@ -0,0 +1,12 @@
+const seconds = new Date().getSeconds();
+
+console.log('Starting up...');
+console.log(`Current second is : ${seconds}`)
+
+if (seconds % 3 === 0) {
+  console.log('\tCurrent second is evenly divisible by three');
+} else if (seconds % 3 === 1) {
+  console.log('\tCurrent second has a remainder of one')
+} else if (seconds % 3 === 2) {
+  console.log('\tCurrent second has a remainder of two');
+}
