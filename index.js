d5b63ea43e2f59f02522a625889f7e637a33f752
SR <C@C.com>
2023-11-18 13:14:57 -0600

    Resolved request with #6 -added period at the end of sentence.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ console.log(`Current second is : ${seconds}`)
 if (seconds % 3 === 0) {
   console.log('\tCurrent second is evenly divisible by three');
 } else if (seconds % 3 === 1) {
-  console.log(`\t${new Date().toISOString()} : Current second has a remainder of one`);
+  console.log(`\t${new Date().toISOString()} : Current second has a remainder of one.`);
 } else if (seconds % 3 === 2) {
   console.log('\tCurrent second has a remainder of two');
 }
