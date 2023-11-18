fda56eff469d467bdc2169ad7bb5c88b96d186d0
Beezyboys <147273107+Beezyboys@users.noreply.github.com>
2023-11-18 13:01:01 -0600

    my commit^C

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ console.log('Starting up...');
 console.log(`Current second is : ${seconds}`)
 
 if (seconds % 3 === 0) {
-  console.log('\tCurrent second is evenly divisible by three');
+  console.log(`\t${new Date().toISOString()} : Current second is evenly divisible by three`);
 } else if (seconds % 3 === 1) {
   console.log('\tCurrent second has a remainder of one')
 } else if (seconds % 3 === 2) {
